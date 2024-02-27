d00f6b488660da2062820d950b81f2d267bff46e
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-02-27 13:08:50 +0530

    practise (introduction to useReducer hook)

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,25 +1,35 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(state, action) {
+  console.log(state + action);
+  if (action.type === "dec") return state - 1;
+  if (action.type === "inc") return state + 1;
+  if (action.type === "defineCount") return state + action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
-
+  const [count, dispatch] = useReducer(reducer, 0);
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: "dec" });
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: "inc" });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: "defineCount", payload: Number(e.target.value) });
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +37,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
