af150d771df664e19d23729c461949540cda3a51
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-02-27 15:29:44 +0530

    using usereducer (practise)

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,16 +1,30 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
   console.log(state + action);
-  if (action.type === "dec") return state - 1;
-  if (action.type === "inc") return state + 1;
-  if (action.type === "defineCount") return state + action.payload;
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknown Action");
+  }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // creating a complex state using useReducer hook
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
@@ -28,17 +42,20 @@ function DateCounter() {
   };
 
   const defineCount = function (e) {
-    dispatch({ type: "defineCount", payload: Number(e.target.value) });
+    dispatch({ type: "setCount", payload: Number(e.target.value) });
     // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
+    // we can update multiple states at the same time using reducer
+    dispatch({ type: "reset" });
     // setCount(0);
-    setStep(1);
+    // setStep(1);
   };
 
   return (
