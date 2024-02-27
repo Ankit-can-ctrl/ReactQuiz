fd533cc0cba3b955a701067d51119fe13d6d4228
Ankit-can-ctrl <128883341+Ankit-can-ctrl@users.noreply.github.com>
2024-02-27 22:00:06 +0530

    creating our own local api and using it to fetch data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,47 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./main";
+import { useReducer } from "react";
+
+const initialstate = {
+  questions: [],
+  // "loading",active,error,ready,finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "datafailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unkown action");
+  }
+}
+
 export default function App() {
-  return <DateCounter />;
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  const [state, dispatch] = useReducer(reducer, initialstate);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
+    </div>
+  );
 }
